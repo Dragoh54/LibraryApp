22e8c09ba18fcab15640940a7f03cbd8d7aa25f5
Dragoh54 <ndragoh@gmail.com>
2024-10-18 14:49:47 +0300

    feat: add Applicaion project

--- LibraryApp.Api/LibraryApp.DataAccess/UnitOfWork/UnitOfWork.cs
+++ LibraryApp.Api/LibraryApp.DataAccess/UnitOfWork/UnitOfWork.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LibraryApp.Application.UnitOfWork;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,6 +7,6 @@ using System.Threading.Tasks;
 
 namespace LibraryApp.DataAccess.UnitOfWork;
 
-public class UnitOfWork 
+public class UnitOfWork : IUnitOfWork
 {
 }
