c90d7ed5ce48bcb054353ae0b1cb1f26b82c64c8
Dragoh54 <ndragoh@gmail.com>
2024-10-22 13:12:02 +0300

    feat: add book and autor to mappingConfig

--- LibraryApp.Api/LibraryApp.Application/Mapper/MappingConfig.cs
+++ LibraryApp.Api/LibraryApp.Application/Mapper/MappingConfig.cs
@@ -17,5 +17,19 @@ public class MappingConfig
             .Map(dest => dest.Nickname, src => src.Nickname)
             .Map(dest => dest.Email, src => src.Email)
             .Map(dest => dest.Role, src => src.Role);
+
+        TypeAdapterConfig<BookEntity, BookDto>.NewConfig()
+            .Map(dest => dest.ISBN, src => src.ISBN)
+            .Map(dest => dest.Description, src => src.Description)
+            .Map(dest => dest.Genre, src => src.Genre)
+            .Map(dest => dest.Title, src => src.Title)
+            .Map(dest => dest.TakenAt, src => src.TakenAt)
+            .Map(dest => dest.ReturnBy, src => src.ReturnBy);
+
+        TypeAdapterConfig<AuthorEntity, AuthorDto>.NewConfig()
+            .Map(dest => dest.Surname, src => src.Surname)
+            .Map(dest => dest.Country, src => src.Country)
+            .Map(dest => dest.BirthDate, src => src.BirthDate)
+            .Map(dest => dest.Books, src => src.Books);
     }
 }
